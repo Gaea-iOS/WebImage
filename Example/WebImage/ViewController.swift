94b22532cf9fc650d4f19815e83dbfd3b637e6de
jerry <445242970@qq.com>
2017-08-23 17:13:33 +0800

    first commit

--- Example/WebImage/ViewController.swift
+++ Example/WebImage/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import WebImage
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let url = URL(string: "http://ouo6mncln.bkt.clouddn.com/3DFCB247-7233-4F5B-9C25-8AFAFA976B3F.webp")
+        //
+        //        downloader.dowload(with: url, progress: { (p1, p2) in
+        //            print("p1 = \(p1), p2 = \(p2)")
+        //        }, success: { (image, url) in
+        //            print("success: url = \(url)")
+        //        }) { (error) in
+        //            print("error: \(error)")
+        //        }
+        
+        imageView.backgroundColor = .red
     }
 
     override func didReceiveMemoryWarning() {
