d778f731c5289eec8e6563f6571b150fb96cbe52
jerry <445242970@qq.com>
2017-08-23 17:59:51 +0800

    add DefaultImageDownloader

--- ImageDownloaderType.swift
+++ ImageDownloaderType.swift
@@ -6,4 +6,28 @@
 //
 //
 
-import Foundation
+import Kingfisher
+
+public protocol ImageDownloaderType {
+    func donwloadImage(with url: URL,
+                       progress: ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)?,
+                       success: ((UIImage) -> Void)?,
+                       failure: ((NSError) -> Void)?)
+}
+
+extension ImageDownloaderType {
+    public func donwloadImage(with url: URL,
+                              progress: ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)? = nil,
+                              success: ((UIImage) -> Void)? = nil,
+                              failure: ((NSError) -> Void)? = nil) {
+        ImageDownloader.default.downloadImage(with: url, progressBlock: progress) { (image, error, _, _) in
+            if let image = image {
+                success?(image)
+            } else {
+                failure?(error!)
+            }
+        }
+    }
+}
+
+public class DefaultImageDownloader: ImageDownloaderType {}
